5bc4dd12dd2b8a93a091b030d375beab29d20aa4
Eugene Batogov <johnbat26@gmail.com>
2013-09-11 01:25:19 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HostedForums::Application.config.secret_key_base = 'b7bbf01a3a257198555ba5703b3dcf0efba5ed1385e515418ff92471194c616ae922b4fa93e5b4f54194f670579410a6edada5c2276740e0d4022fd4e1f0e5c9'
---
+++ db/migrate/20130910211311_add_subdomain_to_subscribem_accounts.subscribem.rb
@@ -0,0 +1,7 @@
+# This migration comes from subscribem (originally 20130907115223)
+class AddSubdomainToSubscribemAccounts < ActiveRecord::Migration
+  def change
+    add_column :subscribem_accounts, :subdomain, :string
+    add_index :subscribem_accounts, :subdomain
+  end
+end
